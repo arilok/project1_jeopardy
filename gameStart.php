9cdc3b49f7601114eb3a6564832ddd369de66bb8
arilok <44273343+arilok@users.noreply.github.com>
2019-03-07 18:57:05 -0500

    Add files via upload
    gameStart is the main playing board.
    home is the welcome page where it asks the user for their name.

---
+++ gameStart.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang = "en">
+   
+<head>
+    <meta charset = "UTF-8">
+	<link href="stylesheet.css" rel="stylesheet" type="text/css">
+    <title>Wheel of Fortune</title>
+</head>
+
+<body>
+	<h1> Jeopredy! </h1>
+	<h2> Welcome $name </h2>
+
+	<table>
+		<tr>
+			<th> Topic 1 </th>
+			<th> Topic 2 </th>
+			<th> Topic 3 </th>
+			<th> Topic 4 </th>
+			<th> Topic 5 </th> 
+			<th> Topic 6 </th>
+		</tr>
+
+		<tr>
+			<td> $200 </td>
+			<td> $200 </td>
+			<td> $200 </td>
+			<td> $200 </td>
+			<td> $200 </td>
+			<td> $200 </td>
+		</tr>
+
+		<tr>
+			<td> $400 </td>
+			<td> $400 </td>
+			<td> $400 </td>
+			<td> $400 </td>
+			<td> $400 </td>
+			<td> $400 </td>
+		</tr>
+
+		<tr>
+			<td> $600 </td>
+			<td> $600 </td>
+			<td> $600 </td>
+			<td> $600 </td>
+			<td> $600 </td>
+			<td> $600 </td>
+		</tr>
+
+		<tr>
+			<td> $800 </td>
+			<td> $800 </td>
+			<td> $800 </td>
+			<td> $800 </td>
+			<td> $800 </td>
+			<td> $800 </td>
+		</tr>
+
+		<tr>
+			<td> $1000 </td>
+			<td> $1000 </td>
+			<td> $1000 </td>
+			<td> $1000 </td>
+			<td> $1000 </td>
+			<td> $1000 </td>
+		</tr> 
+	</table>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang = "en">
+   
+<head>
+    <meta charset = "UTF-8">
+	<link href="stylesheet.css" rel="" ="stylesheet" type="text/css">
+    <title>Wheel of Fortune</title>
+</head>
+
+<body>
+	<?php 
+		echo "Welcome to ___ jeopardy";
+	?>
+		<h1> Please type in your name: <input type = "string" name = "player_name"> <input type = "submit" value = "submit" name = "submit"></h1>
+
+
+</body>
+</html>
